f3d4930ad374ca45598817ffe1f69e0d6e2361b8
Aurélien Christman <christman.aurelien@bcgplatinion.com>
2018-11-20 11:32:32 +0100

    Initial Commit

---
+++ GooglePhotosAPITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePhotosAPITest
+//
+//  Created by Christman, Aurelien (BCG Platinion) on 20/11/2018.
+//  Copyright © 2018 Christman, Aurelien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GooglePhotosAPITestTests/GooglePhotosAPITestTests.swift
@@ -0,0 +1,36 @@
+//
+//  GooglePhotosAPITestTests.swift
+//  GooglePhotosAPITestTests
+//
+//  Created by Christman, Aurelien (BCG Platinion) on 20/11/2018.
+//  Copyright © 2018 Christman, Aurelien. All rights reserved.
+//
+
+import XCTest
+@testable import GooglePhotosAPITest
+
+class GooglePhotosAPITestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
